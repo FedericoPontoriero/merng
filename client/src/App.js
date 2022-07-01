d8094fac4c059a60e5ee85b1687fde7739ecbbe5
Federico <federicolucaspontoriero@gmail.com>
2022-07-01 03:07:29 -0300

    added components and mutations

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
-import Clients from './components/Clients';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project from './pages/Project';
 
 const cache = new InMemoryCache({
 	typePolicies: {
@@ -30,10 +33,16 @@ function App() {
 	return (
 		<>
 			<ApolloProvider client={client}>
-				<Header />
-				<div className='container'>
-					<Clients />
-				</div>
+				<Router>
+					<Header />
+					<div className='container'>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/projects/:id' element={<Project />} />
+							<Route path='*' element={<NotFound />} />
+						</Routes>
+					</div>
+				</Router>
 			</ApolloProvider>
 		</>
 	);
