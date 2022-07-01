27e63e641b6734559f0bf3d6402a806be688e528
Federico <federicolucaspontoriero@gmail.com>
2022-07-01 01:33:14 -0300

    added modal and clients components

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+connectDB();
+app.use(cors());
+
 app.use(
 	'/graphql',
 	graphqlHTTP({
